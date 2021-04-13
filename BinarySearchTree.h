0bd8d7f8c3eaba91040d036132639f8037656f45
Nhahan1997 <70328135+Nhahan1997@users.noreply.github.com>
2021-04-12 22:13:59 -0500

    Add files via upload

--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -24,7 +24,7 @@ class BinaryTreeMemory : BinaryTreeException {};
 class BinaryTreeEmptyTree : BinaryTreeException {};
 
 class BinarySearchTreeException: BinaryTreeException {};
-class BinarySearchTreeChangedSubtree : BinarySearchTreeException {};
+//class BinarySearchTreeChangedSubtree : BinarySearchTreeException {};
 class BinarySearchTreeNotFound : BinarySearchTreeException {};
 
 template <class DataType>
@@ -44,6 +44,7 @@ protected:
 	BinarySearchTree<DataType>* _left;	//left child of root
 	BinarySearchTree<DataType>* _right;	//right child of root
 	BinarySearchTree<DataType>* _yTree;
+	int ID;
 	bool _subtree;	//subtree
 	virtual BinarySearchTree<DataType>* makeSubtree();	//create a new subtree
 	virtual void copyTree (BinarySearchTree<DataType>* bst);
@@ -66,25 +67,40 @@ public:
     // returns the left subtree
 	virtual BinarySearchTree<DataType>* right ();
     // returns the right subtree
+	virtual BinarySearchTree<DataType>* yTree ();
+	// returns the yTree of xNode
 	virtual bool subtree();
 	virtual void makeEmpty ();
 	virtual DataType find (const DataType& q);
 	virtual void remove (const DataType& data);
 	virtual void rangeSearch (const DataType& low, const DataType& high);
-	virtual void insert (const DataType& data);
-	virtual void balanceXTree();
-	virtual void balanceYTree(const DataType& data);
+	virtual BinarySearchTree<DataType>* _insert (const DataType& data);
+	virtual void insert (const DataType& x, const DataType& y, int ID);
+	virtual int getInorderTraversal(BinarySearchTree<DataType>* node,
+			BinarySearchTree<DataType>** inOrderArray, int index);
+	virtual BinarySearchTree<DataType>* GlobalRebalance(BinarySearchTree<DataType>** inOrderArray, int l, int r);
 	virtual void print();
+	virtual void inOrder_xTreeDisplay();
+	virtual void inOrder_yTreeDisplay();
+	virtual void preOrder_xTreeDisplay();
+	virtual void preOrder_yTreeDisplay();
+
+
 
 };
 
 //print BST in in-order traversal
 template <class DataType>
 ostream& operator<< (ostream& s,  BinarySearchTree<DataType>& X) {
-
-	if (!X.isEmpty()) {
-		s << *(X.left ()) << " " << X.root () << " " << *(X.right());
-	}
+//	if (!X.isEmpty()){
+//	s << *(X.left()) << " " << X.root() << " " << *(X.right()) ;
+//	}
+	cout << "INORDER  TRAVERSAL: ";
+	cout << endl;
+	X.inOrder_xTreeDisplay();
+	cout << "PREORDER  TRAVERSAL: ";
+	cout << endl;
+	X.preOrder_xTreeDisplay();
 	return s;
 }
 
@@ -96,6 +112,7 @@ BinarySearchTree<DataType>::BinarySearchTree ()
 	_left = NULL;
 	_right = NULL;
 	_yTree = NULL;
+	ID = 0;
 	_subtree = false;
 }
 // --------------------------------------------------------------
@@ -107,6 +124,54 @@ BinarySearchTree<DataType>::BinarySearchTree (const DataType& data)
 	//if (data == NULL) throw BinaryTreeMemory();
 	_left = makeSubtree ();
 	_right = makeSubtree ();
+	_yTree = NULL;
+	ID = 0;
+}
+// --------------------------------------------------------------
+template <class DataType>
+void BinarySearchTree<DataType>::inOrder_xTreeDisplay(){
+	if (!this->left()->isEmpty()){
+		this->left()->inOrder_xTreeDisplay();
+	}
+	cout << this->root() << ": " << endl;
+	if (!this->yTree()->isEmpty()){
+		this->yTree()->inOrder_yTreeDisplay();
+	}
+	if (!this->right()->isEmpty()){
+		this->right()->inOrder_xTreeDisplay();
+	}
+}
+// --------------------------------------------------------------
+template <class DataType>
+void BinarySearchTree<DataType>::inOrder_yTreeDisplay(){
+	if (!this->left()->isEmpty()){
+		this->left()->inOrder_yTreeDisplay();
+	}
+	cout << " " << this->root() << "(" << this->ID << ") ";
+	cout << "hello";
+	if (!this->right()->isEmpty()){
+		this->right()->inOrder_yTreeDisplay();
+	}
+}
+//--------------------------------------------------------------
+template <class DataType>
+void BinarySearchTree<DataType>::preOrder_xTreeDisplay(){
+	cout << this->root() << ": " << endl;
+	this->yTree()->inOrder_yTreeDisplay();
+	if (!this->left()->isEmpty()){
+		this->left()->inOrder_xTreeDisplay();
+	}
+	if (!this->right()->isEmpty()){
+		this->right()->inOrder_xTreeDisplay();
+	}
+}
+// --------------------------------------------------------------
+template <class DataType>
+void BinarySearchTree<DataType>::preOrder_yTreeDisplay(){
+	cout << " " << this->root() << "(" << this->ID << ") ";
+	this->yTree()->left()->inOrder_yTreeDisplay();
+	this->yTree()->right()->inOrder_yTreeDisplay();
+
 }
 // --------------------------------------------------------------
 template <class DataType>
@@ -129,6 +194,9 @@ BinarySearchTree<DataType>::~BinarySearchTree ()
 	if (_right != NULL)
         delete _right;
     _right = NULL;
+    if (_yTree != NULL)
+       	delete _yTree;
+    _yTree = NULL;
 }
 // --------------------------------------------------------------
 template <class DataType>
@@ -138,6 +206,8 @@ bool BinarySearchTree<DataType>::isEmpty()
 	if (_root == NULL)
 		return true;
 	else return false;
+
+	return true;
 }
 // --------------------------------------------------------------
 template <class DataType>
@@ -174,12 +244,16 @@ BinarySearchTree<DataType>* BinarySearchTree<DataType>::right (){ return _right;
 // returns the right subtree
 // --------------------------------------------------------------
 template <class DataType>
+BinarySearchTree<DataType>* BinarySearchTree<DataType>::yTree (){ return _yTree; }
+// returns the yTree of xNode
+// --------------------------------------------------------------
+template <class DataType>
 bool BinarySearchTree<DataType>::subtree() { return _subtree; }
 // --------------------------------------------------------------
 template <class DataType>
 void BinarySearchTree<DataType>::makeEmpty ()
 {
-	if (_subtree) throw BinarySearchTreeChangedSubtree();
+	//if (_subtree) throw BinarySearchTreeChangedSubtree();
 	if (_root != NULL)
    		delete _root;
     _root = NULL;
@@ -189,6 +263,9 @@ void BinarySearchTree<DataType>::makeEmpty ()
 	if (_right != NULL)
 	   	delete _right;
     _right = NULL;
+    if (_yTree != NULL)
+    	delete _yTree;
+    _yTree = NULL;
 }
 // --------------------------------------------------------------
 template <class DataType>
@@ -244,16 +321,17 @@ DataType BinarySearchTree<DataType>::find (const DataType& q)
 	}
 	catch(Exception e)
 	{
-		cout<<"\nSorry!!! Element is not found in the tree";//<<endl;
-		cout<<endl;
-		return q; //We need to fix this
+//		cout<<"\nSorry!!! Element is not found in the tree";//<<endl;
+//		cout<<endl;
+//		return q; //We need to fix this
+		return -1;
 	}
 }
 // --------------------------------------------------------------
 template <class DataType>
-void BinarySearchTree<DataType>::insert (const DataType& data)
+BinarySearchTree<DataType>* BinarySearchTree<DataType>::_insert (const DataType& data)
 {
-	if (_subtree) throw BinarySearchTreeChangedSubtree();
+	//if (_subtree) throw BinarySearchTreeChangedSubtree();
 	BinarySearchTree<DataType>* bst = _find (data);
 	if (bst->isEmpty())
 	{
@@ -266,12 +344,44 @@ void BinarySearchTree<DataType>::insert (const DataType& data)
         delete bst->_root;
 		bst->_root = new DataType (data);
 	}
+	return bst;
+}
+// --------------------------------------------------------------
+template <class DataType>
+void BinarySearchTree<DataType>::insert (const DataType& x, const DataType& y, int IDT)
+{
+	BinarySearchTree<DataType>* searchBST = _find(x);
+	if (searchBST->isEmpty()){
+		BinarySearchTree<DataType>* temp1 = _insert(x);
+
+		BinarySearchTree<DataType>* yTree = temp1->_yTree;
+		if (yTree == NULL){
+			yTree = new BinarySearchTree<DataType>(y);
+			yTree->ID = IDT;
+		}
+		else{
+			BinarySearchTree<DataType>* temp2 = _insert(y);
+			temp2->ID = IDT;
+		}
+
+	}
+	else{
+		BinarySearchTree<DataType>* yTree = searchBST->_yTree;
+		if (yTree == NULL){
+			yTree = new BinarySearchTree<DataType>(y);
+			yTree->ID = IDT;
+		}
+		else{
+			BinarySearchTree<DataType>* temp2 = _insert(y);
+			temp2->ID = IDT;
+		}
+	}
 }
 // --------------------------------------------------------------
 template <class DataType>
 void BinarySearchTree<DataType>::remove (const DataType& data)
 {
-	if (_subtree) throw BinarySearchTreeChangedSubtree();
+	//if (_subtree) throw BinarySearchTreeChangedSubtree();
 	BinarySearchTree<DataType>* bst;
 	BinarySearchTree<DataType>* bst2;
 	BinarySearchTree<DataType>* bst3;
@@ -347,6 +457,65 @@ void BinarySearchTree<DataType>::rangeSearch (const DataType& low, const DataTyp
     }
 	if (*_root <= high)
         _right->rangeSearch(low,high);
+}
+
+// --------------------------------------------------------------
+template <class DataType>
+BinarySearchTree<DataType>* BinarySearchTree<DataType>::GlobalRebalance(BinarySearchTree<DataType>** inOrderArray, int l, int r){
+	int mid = 0;
+	BinarySearchTree<DataType>* temp = new BinarySearchTree<DataType>();
+
+	if(l <= r)
+	{
+		mid = ((l + r) / 2);
+		temp = inOrderArray[mid];
+//		temp->left() = GlobalRebalance(inOrderArray, l, mid - 1);
+//		temp->right() = GlobalRebalance(inOrderArray, mid + 1, r);
+	}
+
+	return temp;
+}
+// --------------------------------------------------------------
+template <class DataType>
+int BinarySearchTree<DataType>::getInorderTraversal(BinarySearchTree<DataType>* node, BinarySearchTree<DataType>** inOrderArray, int index)
+{
+	try{
+		if (node->isEmpty()) throw BinarySearchTreeNotFound();
+	}
+	catch(BinarySearchTreeNotFound e){
+		cout << "No tree for inorder traversal";
+		cout << endl;
+		return index;
+	}
+
+	cout << "value: " << this->root() << endl;
+	cout << "left: " << *(this->left()->_root) << endl;
+	cout << "right: " << *(this->right()->_root) << endl;
+
+	if (!this->isEmpty()) {
+		if(node->left()->_root != NULL)
+		{
+			// cout << "\tcalling left" << endl;
+			index = node->left()->getInorderTraversal(node->left(), inOrderArray, index);
+		}
+
+		inOrderArray[index++] = node;
+		// cout << "index: " << index - 1 << " inOrderArray[i]: " << inOrderArray[index - 1]->root() << endl;
+
+		if(node->right()->_root != NULL)
+		{
+			// cout << "\tcalling right" << endl;
+			index = node->right()->getInorderTraversal(node->right(), inOrderArray, index);
+		}
+	}
+	return index;
+}
+// --------------------------------------------------------------
+template <class DataType>
+void BinarySearchTree<DataType>::print(){
+//	BinarySearchTree<DataType>* bst = this;
+//	cout << *bst << endl;
+	cout << *this << endl;
 }
 
 
--- Main2Bst.cpp
+++ Main2Bst.cpp
@@ -4,219 +4,98 @@
 
 #include "BinarySearchTree.h"
 
-//template <class DT>
-//class xNode;
-
-//template<class DT>
-//class xNode
-//{
-//	protected:
-//		int xCordinate;
-//		int id;
-//		BinarySearchTree<DT>* bst;
-//	public:
-//		xNode();
-//		xNode(const DT& x, const DT& _id);
-//		virtual ~xNode();
-//		virtual void xInsert(const DT& data);
-//		virtual void xRemove(const DT& data);
-//		virtual DT xFind (const DT& q);
-//		virtual void xRangeSearch(const DT& low, const DT& high);
-//		virtual BinarySearchTree<DT>* balanceXTree(const DT& data);
-//		virtual void print();
-//
-//};
-//
-////default constructor
-//template <class DT>
-//xNode<DT>::xNode(){
-//	xCordinate = 0;
-//	id = 0;
-//	bst = new BinarySearchTree<DT>();
-//}
-//
-////non-default constructor
-//template <class DT>
-//xNode<DT>::xNode(const DT& x, const DT& _id){
-//	xCordinate = x;
-//	id = _id;
-//	bst = new BinarySearchTree<DT>();
-//}
-//
-//template <class DT>
-//xNode<DT>::~xNode(){
-//	xCordinate = 0;
-//	id = 0;
-//}
-//
-//template <class DT>
-//void xNode<DT>::xInsert(const DT& data){
-//	bst->insert(data);
-//}
-//
-//template <class DT>
-//void xNode<DT>::xRemove(const DT& data){
-//	bst->remove(data);
-//}
-//
-//template <class DT>
-//DT xNode<DT>::xFind(const DT& q){
-//	bst->find(q);
-//}
-//
-//template <class DT>
-//void xNode<DT>::xRangeSearch(const DT& low, const DT& high){
-//
-//}
-//
-//template <class DT>
-//void xNode<DT>::print(){
-//
-//}
-//
-////yNOde class
-//template<class DT>
-//class yNode
-//{
-//	protected:
-//		int yCordinate;
-//		int id;
-//		BinarySearchTree<DT>* bst;
-//	public:
-//		yNode();
-//		yNode(const DT& y, const DT& _id);
-//		virtual ~yNode();
-//		virtual void yInsert(const DT& data);
-//		virtual void yRemove(const DT& data);
-//		virtual DT yFind (const DT& q);
-//		virtual void yRangeSearch(const DT& low, const DT& high);
-//		virtual BinarySearchTree<DT>* balanceYTree(const DT& data);
-//		virtual void print();
-//
-//};
-//
-////default constructor
-//template <class DT>
-//yNode<DT>::yNode(){
-//	yCordinate = 0;
-//	id = 0;
-//	bst = new BinarySearchTree<DT>();
-//}
-//
-////non-default constructor
-//template <class DT>
-//yNode<DT>::yNode(const DT& y, const DT& _id){
-//	yCordinate = y;
-//	id = _id;
-//	bst = new BinarySearchTree<DT>();
-//}
-//
-//template <class DT>
-//yNode<DT>::~yNode(){
-//	yCordinate = 0;
-//	id = 0;
-//}
-//
-//template <class DT>
-//void yNode<DT>::yInsert(const DT& data){
-//	bst->insert(data);
-//}
-//
-//template <class DT>
-//void yNode<DT>::yRemove(const DT& data){
-//	bst->remove(data);
-//}
-//
-//template <class DT>
-//DT yNode<DT>::yFind(const DT& q){
-//	bst->find(q);
-//}
-//
-//template <class DT>
-//void yNode<DT>::yRangeSearch(const DT& low, const DT& high){
-//
-//}
-//
-//template <class DT>
-//void yNode<DT>::print(){
-//
-//}
-
 int main()
 {
-    BinarySearchTree<int> bst (100);
-
-    cout <<"Initially the tree has  "<< bst << endl;
-
-    bst.insert (20);
-    cout <<"After Insertion of 20:  "<< bst << endl;
-
-    bst.insert (10);
-    cout <<"After Insertion of 10:  "<< bst << endl;
-
-    bst.insert (200);
-    cout <<"After Insertion of 200:  "<< bst << endl;
-
-    bst.insert (25);
-    cout <<"After Insertion of 25:  "<< bst << endl;
-
-    cout << "Trying to find 30..." << endl;
-    try {
-            bst.find(30);
-    }
-    catch (BinarySearchTreeNotFound e) {
-        cout << "Element 30 not found!!" << endl;
-    }
-
-    bst.remove(25);
-    cout<<"After removal of 25:"<<bst<<endl;
-
-    cout << "Trying to find 25..." << endl;
-    try {
-            bst.find(25);
-    }
-    catch (BinarySearchTreeNotFound e) {
-        cout << "Element 25 not found!!" << endl;
-    }
-
-    bst.remove(100);
-    cout<<"After removal of 100:"<<bst<<endl;
-
-    bst.remove(20);
-    cout<<"After removal of 20:"<<bst<<endl;
-
-    bst.remove(10);
-    cout<<"After removal of 10:"<<bst<<endl;
-
-//    bst.insert(5);
+//    BinarySearchTree<int> bst (100);
+//
+//    cout <<"Initially the tree has  "<< bst << endl;
+//
+//    bst._insert (20);
+//    cout <<"After Insertion of 20:  "<< bst << endl;
+//
+//    bst._insert (10);
+//    cout <<"After Insertion of 10:  "<< bst << endl;
+//
+//    bst._insert (200);
+//    cout <<"After Insertion of 200:  "<< bst << endl;
+//
+//    bst._insert (25);
+//    cout <<"After Insertion of 25:  "<< bst << endl;
+//
+//    cout << "Trying to find 30..." << endl;
 //    try {
-//                bst.find(5);
-//        }
-//        catch (BinarySearchTreeNotFound e) {
-//            cout << "Element 5 not found!!" << endl;
-//        }
+//            bst.find(30);
+//    }
+//    catch (BinarySearchTreeNotFound e) {
+//        cout << "Element 30 not found!!" << endl;
+//    }
+//
+//   // bst.remove(25);
+//    cout<<"After removal of 25:"<<bst<<endl;
+//
+//    cout << "Trying to find 25..." << endl;
+//    try {
+//            bst.find(25);
+//    }
+//    catch (BinarySearchTreeNotFound e) {
+//        cout << "Element 25 not found!!" << endl;
+//    }
+//
+//    bst.remove(100);
+//    cout<<"After removal of 100:"<<bst<<endl;
+//
+//    bst.remove(20);
+//    cout<<"After removal of 20:"<<bst<<endl;
+//
+//    bst.remove(10);
+//    cout<<"After removal of 10:"<<bst<<endl;
 
 
-    int letter;
+    char letter;
     int xCor;
     int yCor;
     int id;
+    int low, high, lowY, highY;
     int size = 100; //number of line in input file
 
-   BinarySearchTree<int>* newBst();
-//    xNode<int>* xVal;
-//    yNode<int>* yVal;
+   BinarySearchTree<int>* newBst = new BinarySearchTree<int>();
+
    for(int i = 0; i < size; i++){
 	   if (i == size - 1)
 		   size++;	//expand size to read new line
 
-	   cin >> letter >> xCor >> yCor >> id;
+	   cin >> letter;
 	   if (letter == 'I'){
+		   cin >> xCor >> yCor >> id;
+		   (*newBst).insert(xCor, yCor, id);
+		   cout << xCor << " " << yCor << " " << id << " " << endl;
 
+	   }
+	   if (letter == 'R'){
+		   cout << "Remove: ";
+		   cin >> low >> high;
+		   cout << low << " " << high << endl;
+
+	   }
+	   if (letter == 'S'){
+		   cout << "Range search" << endl;
+		   cin >> low >> high >> lowY >> highY;
+		   cout << low << " " << high;
+		   cout << lowY << " " << highY << endl;
+
+	   }
+	   if (letter == 'Y'){
+		   cout << "Y tree balance" << endl;
+		   cin >> yCor;
+		   cout << yCor << endl;
+
+	   }
+	   if (letter == 'P'){
+		   (*newBst).print();
 	   }
 
     }
 
-
+   	delete newBst;
     return 0;
 }
